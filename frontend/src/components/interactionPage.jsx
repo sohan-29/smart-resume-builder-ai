31ae47d499e374cb78012103a554c5de99cb2d0b
Sohan <sohan291106@gmail.com>
2025-07-09 11:15:43 +0530

    modified style for selected and recommended fields

--- frontend/src/components/interactionPage.jsx
+++ frontend/src/components/interactionPage.jsx
@@ -38,32 +38,32 @@ const UserInteractionPage = ({ userDataSet, setUserDataSet }) => {
     <>
       <Header />
       <div className="bg-gray-100 p-6 flex flex-col gap-6">
-        <h2 className="text-xl font-semibold mb-4">Selected Fields</h2>
-        <div className="flex flex-wrap w-full p-4 space-y-4">
+        <h2 className="text-xl md:text-3xl font-bold mb-4">Selected Fields :</h2>
+        <div className="flex flex-wrap gap-4 w-full bg-[#11111169] pt-8 px-8 pb-4 rounded-xl min-h-44 space-y-4">
           {selectedFields.map((field, index) => {
             return (
-              <div key={index} className="p-4 bg-white rounded shadow mb-4 w-fit">
+              <div key={index} className="flex items-center bg-[#00000029] rounded-full pl-6 shadow mb-4 h-fit w-fit">
                 <h2 className="text-xl font-semibold">{field}</h2>
-                <button
+                  <button
                   onClick={() => handleRemovedField(field, index)}
-                  className="mt-2 px-4 py-2 bg-red-500 text-white rounded"
+                  className="mb-1 mt-2 pr-3 px-1.5 h-9 w-9 cursor-pointer group"
                 >
-                  Remove
+                  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="group-hover:fill-white transition-colors duration-300"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fillRule="evenodd" clipRule="evenodd" d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z" fill="#ffffff96"></path></g></svg>
                 </button>
               </div>
             );
           })}
         </div>
-        <h2 className="text-xl font-semibold mb-4">Recommended Fields</h2>
-        <div className="flex flex-wrap w-full p-4 space-y-4">
+        <h2 className="text:xl md:text-3xl font-bold mb-4">Recommended Fields :</h2>
+        <div className="flex flex-wrap gap-4 w-full bg-[#11111169] pt-8 px-8 pb-4 rounded-xl min-h-44 space-y-4">
           {recommendedFields.map((field, index) => (
-            <div key={index} className="p-4 bg-white rounded shadow mb-4">
+            <div key={index} className="flex items-center bg-[#00000029] rounded-full pl-6 shadow mb-4 h-fit w-fit">
               <h3 className="text-lg font-semibold">{field}</h3>
               <button
                 onClick={() => handleAddedField(field, index)}
-                className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
+                className="mb-1 mt-2 pr-3 px-1.5 h-10 w-10 cursor-pointer group"
               >
-                Add
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" className="group-hover:stroke-white transition-colors duration-300"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g data-name="add" id="add-2"> <g> <line fill="none" stroke="#ffffff99" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="12" x2="12" y1="19" y2="5"></line> <line fill="none" stroke="#ffffff96" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="5" x2="19" y1="12" y2="12"></line> </g> </g> </g> </g></svg>
               </button>
             </div>
           ))}
